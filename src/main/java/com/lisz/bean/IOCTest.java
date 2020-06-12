62156881ec7f43b4181a11385aab86ca5954b269
lisz1012 <lisz1012@163.com>
2020-06-12 00:18:45 -0700

    Add notes

--- src/main/java/com/lisz/bean/IOCTest.java
+++ src/main/java/com/lisz/bean/IOCTest.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 // 容器中的对象是什么时候创建的？
-// 在容器创建完成之前就创建好了
+// 在容器创建完成之前就创建好了（懒加载先不考虑）
 public class IOCTest {
     public static void main(String[] args) {
         // 对于Maven项目，要把ioc.xml放在resources资源目录里才能找得到. ApplicationContext是IOC容器的入口
