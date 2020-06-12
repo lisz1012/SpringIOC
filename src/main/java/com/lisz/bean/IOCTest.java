7a76b05007fee1978d04d1a529e6404adaf909d9
lisz1012 <lisz1012@163.com>
2020-06-11 23:53:59 -0700

    Add notes

--- src/main/java/com/lisz/bean/IOCTest.java
+++ src/main/java/com/lisz/bean/IOCTest.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class IOCTest {
     public static void main(String[] args) {
+        // 对于Maven项目，要把ioc.xml放在resources资源目录里才能找得到
         ApplicationContext context = new ClassPathXmlApplicationContext("ioc.xml");
         Person person = context.getBean("person", Person.class);
         System.out.println(person);
