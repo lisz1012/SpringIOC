1f5699adface14e3b8edaba212ece7d75138d08d
lisz1012 <lisz1012@163.com>
2020-06-12 22:13:18 -0700

    xml注入属性

--- src/main/java/com/lisz/bean/IOCTest.java
+++ src/main/java/com/lisz/bean/IOCTest.java
@@ -9,9 +9,21 @@ public class IOCTest {
     public static void main(String[] args) {
         // 对于Maven项目，要把ioc.xml放在resources资源目录里才能找得到. ApplicationContext是IOC容器的入口
         // 创建该类才能拿对象，他有两个子类实现：ClassPathXmlApplicationContext表示从classpath读数据，常用
-        // FileSystemXmlApplicationContext表示从当前文件系统读数据
+        // FileSystemXmlApplicationContext表示从当前文件系统读数据。默认容器启动之后Person对象就已经创建完成了
         ApplicationContext context = new ClassPathXmlApplicationContext("ioc.xml");
         Person person = context.getBean("person", Person.class);
         System.out.println(person);
+        Person person2 = context.getBean("person", Person.class);
+        System.out.println(person == person2); // 默认是单例true，除非bean标签里面写了 scope="prototype".实际就是Map存储beans
+        // scope="prototype"的时候会"懒加载"
+        //Person p = context.getBean(Person.class);// 多个Person类型的不同id的bean的时候，会报错，Spring不知道该注入哪一个
+        Person person3 = context.getBean("person3", Person.class);
+        System.out.println(person3);
+        Person person4 = context.getBean("person4", Person.class);
+        System.out.println(person4);
+        Person person5 = context.getBean("person5", Person.class);
+        System.out.println(person5);
+        Person person6 = context.getBean("person6", Person.class);
+        System.out.println(person6);
     }
 }
--- src/main/java/com/lisz/bean/Person.java
+++ src/main/java/com/lisz/bean/Person.java
@@ -1,15 +1,43 @@
 package com.lisz.bean;
 
+import java.util.Date;
+
 public class Person {
     private int id;
     private String name;
-    private String age;
+    private int age;
     private String gender;
+    private Date date;
 
+    // 当从从其中获取对象的时候，最好是保留无参数的构造方法，因为Spring是通过反射初始化bean的
     public Person() {
         System.out.println("Person 被创建");
     }
 
+    public Person(int id, String name, int age, String gender, Date date) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.date = date;
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        System.out.println("age...");
+    }
+
+    public Person(int id, String name, String gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        System.out.println("gender...");
+    }
+
+    // 用配置文件进行容器中对象初始化的时候setter不能省略，否则xml文件报错，因为它是通过setter的setXxx中的xxx注入，但是找不到setter
+    // bean中要写id="xxx"，一般自动生成的setter没有这个问题
     public void setId(int id) {
         this.id = id;
     }
@@ -18,7 +46,7 @@ public class Person {
         this.name = name;
     }
 
-    public void setAge(String age) {
+    public void setAge(int age) {
         this.age = age;
     }
 
@@ -34,7 +62,7 @@ public class Person {
         return name;
     }
 
-    public String getAge() {
+    public int getAge() {
         return age;
     }
 
@@ -42,13 +70,22 @@ public class Person {
         return gender;
     }
 
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", age='" + age + '\'' +
+                ", age=" + age +
                 ", gender='" + gender + '\'' +
+                ", date=" + date +
                 '}';
     }
 }
